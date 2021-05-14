d2818ba624c9e45f38d1d57181f65616a72a30ba
Berrysoft <Strawberry_Str@hotmail.com>
2021-05-14 23:39:54 +0800

    Fix for clippy.

--- src/mach.rs
+++ src/mach.rs
@@ -58,7 +58,7 @@ impl<'a> MachAnalyzer<'a> {
                 gmach::Mach::Binary(bin) => vec![MachOAnalyzer::from_bin(bin)],
                 gmach::Mach::Fat(multi) => multi
                     .into_iter()
-                    .filter_map(|bin| bin.ok().map(|bin| MachOAnalyzer::from_bin(bin)))
+                    .filter_map(|bin| bin.ok().map(MachOAnalyzer::from_bin))
                     .collect(),
             },
         }
