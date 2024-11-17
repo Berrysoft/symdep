1c2c7364a3dbae03fc06ed0a667534ef83479f77
王宇逸 <Strawberry_Str@hotmail.com>
2024-11-18 03:20:05 +0900

    Add freebsd support.

--- build.rs
+++ build.rs
@@ -2,6 +2,6 @@ use cfg_aliases::cfg_aliases;
 
 fn main() {
     cfg_aliases! {
-        elf: { any(target_os = "linux", target_os = "illumos", target_os = "solaris") }
+        elf: { any(target_os = "linux", target_os = "freebsd", target_os = "illumos", target_os = "solaris") }
     }
 }
